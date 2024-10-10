dde272d0ea7e03613240c536926addf5818d8d60
François Caremoli <francois.caremoli@wanadoo.fr>
2024-10-10 09:02:59 +0200

    Ajout correction TP Streams

---
+++ main-module/src/main/java/com/bigcorp/project/main/correction/Child.java
@@ -0,0 +1,30 @@
+package com.bigcorp.project.main.correction;
+
+public class Child {
+
+	private String nom;
+
+	private Integer age;
+
+	public Child(String nom, Integer age) {
+		this.nom = nom;
+		this.age = age;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+}
